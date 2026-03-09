12c80c2d1c320421154bb3178d40be7f6716baa9
Yuvraj Rajni Sachin Deshmukh <yuvraj280605@gmail.com>
2026-03-09 19:58:20 +0530

    240

--- Medium/240.Search a 2D Matrix II.cpp
+++ Medium/240.Search a 2D Matrix II.cpp
@@ -1,4 +1,22 @@
-
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        int r = 0;
+        int c = m - 1;
+        while(c >= 0 && r < n){
+            if(target == matrix[r][c]){
+                return true;
+            }else if(target < matrix[r][c]){
+                c--;
+            }else{
+                r++;
+            }
+        }
+        return false;
+    }
+};
 //
 // Created by Yuvraj Rajni Sachin Deshmukh on 07/03/26.
 //
