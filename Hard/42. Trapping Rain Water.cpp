fa1841c09f9cfa4d35b0d17c4c5a8430441d0338
Yuvraj Rajni Sachin Deshmukh <yuvraj280605@gmail.com>
2026-03-10 22:29:58 +0530

    42

--- Hard/42. Trapping Rain Water.cpp
+++ Hard/42. Trapping Rain Water.cpp
@@ -19,6 +19,31 @@ public:
         return ans;
     }
 };
+
+//Two Pointer approach
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n = height.size();
+        int ans = 0;
+        int l = 0, r = n - 1;
+        int lmax = 0, rmax = 0;
+
+        while(l < r){
+            lmax = max(lmax , height[l]);
+            rmax = max(rmax , height[r]);
+            if(lmax < rmax){
+                ans += (lmax - height[l]);
+                l++;
+            }else{
+                ans += (rmax - height[r]);
+                r--;
+            }
+        }
+        return ans;
+    }
+};
+
 //
 // Created by Yuvraj Rajni Sachin Deshmukh on 09/03/26.
 //
