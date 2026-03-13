309be5286d990c0858369f90852bf91a814e9e8a
Yuvraj Rajni Sachin Deshmukh <yuvraj280605@gmail.com>
2026-03-13 23:00:12 +0530

    125

---
+++ Easy/125.Valid Palindromes.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool isAlphaNum(char ch){
+        if((ch >= '0' && ch <= '9') || (tolower(ch) >= 'a' && tolower(ch) <= 'z')){
+            return true;
+        }
+        return false;
+    }
+    bool isPalindrome(string s) {
+        int st = 0, end = s.length() - 1;
+
+        while(st < end){
+            if(!isAlphaNum(s[st])){
+                st++; continue;
+            }
+            if(!isAlphaNum(s[end])){
+                end--; continue;
+            }
+            if(tolower(s[st]) != tolower(s[end])){
+                return false;
+            }
+            st++ , end--;
+        }
+        return true;
+
+    }
+};
+//
+// Created by Yuvraj Rajni Sachin Deshmukh on 13/03/26.
+//
