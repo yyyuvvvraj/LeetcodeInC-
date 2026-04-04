dc80712b50f91e0901ee4daf0ce3dc29134b09d4
Yuvraj Rajni Sachin Deshmukh <yuvraj280605@gmail.com>
2026-04-04 09:23:53 +0530

    2075

---
+++ Medium/2075. Decode the Slanted Ciphertext.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string decodeCiphertext(string encodedText, int rows) {
+        int l       = encodedText.size();
+        int columns = l / rows;
+
+        string originalText;
+        for (int col = 0; col < columns; col++) {
+            for (int j = col; j < l; j += (columns + 1)) {
+                originalText += encodedText[j];
+            }
+        }
+
+        //Remove spaces put at the end
+        while (!originalText.empty() && originalText.back() == ' ') {
+            originalText.pop_back();
+        }
+
+        return originalText;
+    }
+};
+//
+// Created by Yuvraj Rajni Sachin Deshmukh on 04/04/26.
+//
