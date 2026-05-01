1838c0c60858b2b00f64a172f276f2dd741ede5c
Yuvraj Rajni Sachin Deshmukh <yuvraj280605@gmail.com>
2026-05-01 23:21:48 +0530

    396

---
+++ Medium/396. Rotate Function.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int maxRotateFunction(vector<int>& nums) {
+        int n = nums.size();
+
+        int sum = 0;
+        int F = 0;
+
+        for(int i = 0; i < n; i++) {
+            sum += nums[i];
+            F += i*nums[i];
+        }
+
+        int result = F;
+
+        for(int k = 0; k <= n-1; k++) {
+            int newF = F + sum - n*nums[n-1-k];
+
+            result = max(result, newF);
+
+            F = newF;
+        }
+
+        return result;
+    }
+};
+//
+// Created by Yuvraj Rajni Sachin Deshmukh on 01/05/26.
+//
